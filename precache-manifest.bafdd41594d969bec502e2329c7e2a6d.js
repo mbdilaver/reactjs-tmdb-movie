0a79616f75711a7c13d829d2c02b758757fbe55f
mbdilaver <mbdilaver@gmail.com>
2019-03-01 17:04:00 +0300

    Updates

---
+++ precache-manifest.bafdd41594d969bec502e2329c7e2a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d63a6d1812eee8ce17c9",
+    "url": "/reactjs-tmdb-movie/static/js/runtime~main.d63a6d18.js"
+  },
+  {
+    "revision": "e81f7ff0361dc7da7ff2",
+    "url": "/reactjs-tmdb-movie/static/js/main.e81f7ff0.chunk.js"
+  },
+  {
+    "revision": "81b15ed9b8d53a1acfce",
+    "url": "/reactjs-tmdb-movie/static/js/2.81b15ed9.chunk.js"
+  },
+  {
+    "revision": "e81f7ff0361dc7da7ff2",
+    "url": "/reactjs-tmdb-movie/static/css/main.a02fa463.chunk.css"
+  },
+  {
+    "revision": "81b15ed9b8d53a1acfce",
+    "url": "/reactjs-tmdb-movie/static/css/2.6ac87a3c.chunk.css"
+  },
+  {
+    "revision": "7957722677b53eae9c6a9f76b5b02ea6",
+    "url": "/reactjs-tmdb-movie/index.html"
+  }
+];
